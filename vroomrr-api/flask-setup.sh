74e068128399e8d510620f54658cbc67953226d7
Luuk Esselbrugge <luuk@esselbrugge.com>
2020-12-16 15:26:51 +0100

    Flask & Docker updates, getUser, RegisterUser, Login, Sessions

--- vroomrr-api/flask-setup.sh
+++ vroomrr-api/flask-setup.sh
@@ -2,4 +2,6 @@
 apt-get update && apt-get install -y python3 && apt-get install python3-pip -y && apt-get install uwsgi-plugin-python -y
 /usr/bin/pip3 install Flask
 /usr/bin/pip3 install uwsgi
-/usr/bin/pip3 install mysql-connector
+/usr/bin/pip3 install Flask-SQLAlchemy
+/usr/bin/pip3 install pymysql
+/usr/bin/pip3 install dataclasses-json
