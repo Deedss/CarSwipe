eb044da3ff285f7dff5a78f949f5aa67aee2979b
Deedss <gertjan.rolink@outlook.com>
2020-12-17 20:31:50 +0100

    still working listview, kotlin tutorials are better

--- vroomrr-app/app/src/main/java/com/example/vroomrr/ui/car/CarFragment.java
+++ vroomrr-app/app/src/main/java/com/example/vroomrr/ui/car/CarFragment.java
@@ -1,9 +1,11 @@
 package com.example.vroomrr.ui.car;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -11,31 +13,36 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.vroomrr.Car;
 import com.example.vroomrr.R;
 import com.example.vroomrr.ui.login.LoginViewModel;
+
+import java.util.ArrayList;
 
 public class CarFragment extends Fragment {
     private CarViewModel carViewModel;
     private TextView textView;
     private View root;
 
-    private RecyclerView recyclerView;
+    private ArrayList<Car> cars;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        Log.d("cars: ", "onCreateView: did it work?");
         root = inflater.inflate(R.layout.fragment_car, container, false);
         carViewModel = new ViewModelProvider(this).get(CarViewModel.class);
-        textView = root.findViewById(R.id.text_car);
-        recyclerView = root.findViewById(R.id.recyclerview);
-
-        carViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
 
         return root;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        RecyclerView recyclerView = view.findViewById(R.id.car_recyclerview);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        recyclerView.setAdapter(new CarListViewAdapter(cars));
     }
 }
--- vroomrr-app/app/src/main/java/com/example/vroomrr/ui/car/CarListViewAdapter.java
+++ vroomrr-app/app/src/main/java/com/example/vroomrr/ui/car/CarListViewAdapter.java
@@ -1,29 +1,67 @@
 package com.example.vroomrr.ui.car;
 
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class CarListViewAdapter extends RecyclerView.Adapter{
+import com.example.vroomrr.Car;
+import com.example.vroomrr.R;
 
-    public CarListViewAdapter() {
+import java.util.ArrayList;
+
+public class CarListViewAdapter extends RecyclerView.Adapter<CarListViewAdapter.CarListViewHolder>{
+    ArrayList<Car> cars;
+
+    public CarListViewAdapter(ArrayList<Car> cars) {
+        this.cars = cars;
     }
 
     @NonNull
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+    public CarListViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new CarListViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.car_row, parent, false));
     }
 
     @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
-
+    public void onBindViewHolder(@NonNull final CarListViewHolder holder, final int position) {
+        holder.car_image.setImageResource(cars.get(position).getImageResource());
+        holder.car_name.setText(cars.get(position).getName());
+        holder.car_license.setText(cars.get(position).getLicense());
+        holder.car_deletebtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cars.remove(position);
+            }
+        });
     }
 
     @Override
     public int getItemCount() {
         return 0;
+    }
+
+    public class CarListViewHolder extends RecyclerView.ViewHolder{
+        // Variables
+        ImageView car_image;
+        TextView car_name;
+        TextView car_license;
+        ImageButton car_deletebtn;
+
+        public CarListViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            this.car_image = itemView.findViewById(R.id.car_image);
+            this.car_name = itemView.findViewById(R.id.car_name);
+            this.car_license = itemView.findViewById(R.id.car_license);
+            this.car_deletebtn = itemView.findViewById(R.id.car_deletebtn);
+        }
     }
 }
